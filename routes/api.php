92ec362ea34773e32076c85987ce08467bbaf957
ngarukiyimana sostene <ngarukiyimanasostene@gmail.com>
2022-10-28 19:24:17 +0200

    created user controller and authentication using sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UserController;
 
 
 Route::resource("products",ProductController::class);
 Route::resource("supllier",SupplierController::class);
 
+// public routes
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// product routes
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
+
+// user routes
+Route::post("/register",[UserController::class,'register']);
+Route::post("/login",[UserController::class,'login']);
+Route::post("/logout",[UserController::class,'logout']);
+
+// protected routes
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    // product routes
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/update/{id}',[ProductController::class,'update']);
+    Route::delete('/products/delete/{id}',[ProductController::class,'destroy']);
+
+    // user routes
+    Route::post("/logout",[UserController::class,'logout']);
 });
