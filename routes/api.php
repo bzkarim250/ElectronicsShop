82c0ea6275205e16ea860c0e8ec7b52e3efb6eea
bzkarim250 <bzkarim250@gmail.com>
2022-10-28 18:34:50 +0200

    added all cart CRUD operations

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\CartController;
 
 
 Route::resource("products",ProductController::class);
 Route::resource("supllier",SupplierController::class);
+Route::resource("cart",CartController::class);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
