4c984d085bbbbdeb4ecd059a79b0263abba433f2
bzkarim250 <bzkarim250@gmail.com>
2022-10-28 19:52:24 +0200

    added order controller

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SupplierController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
 
 
 Route::resource("products",ProductController::class);
 Route::resource("supllier",SupplierController::class);
 Route::resource("cart",CartController::class);
+Route::resource("order",OrderController::class);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
