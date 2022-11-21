51050050ead37062cdc559769962576f26a50607
bzkarim250 <bzkarim250@gmail.com>
2022-11-21 19:00:27 +0200

    @ammend

--- resources/views/layouts/container.blade.php
+++ resources/views/layouts/container.blade.php
@@ -145,7 +145,7 @@
 						<!-- LOGO -->
 						<div class="col-md-3">
 							<div class="header-logo">
-								<a href="#" class="logo">
+								<a href="/" class="logo">
 									<img src="./img/logo.png" alt="">
 								</a>
 							</div>
